d4d08ada528ec7c0a7aec64e3891b8faebe066f7
Ahmad Raza <ahmad.eastwest.sah@gmail.com>
2022-09-30 20:01:57 +0500

    data store changed | error handling updated

--- 004_docker/app.py
+++ 004_docker/app.py
@@ -1,57 +1,79 @@
-from flask import Flask, jsonify, request
+import uuid
+from db import stores, items
+from flask import Flask, request
+from flask_smorest import abort
 
 
 app    = Flask(__name__)
-stores = []
 
-#POST /store {name:}
+
 @app.route("/store", methods=['POST'])
 def create_store():
-    request_data= request.get_json()
-    new_store   = {
-                    "name"  : request_data['name'],
-                    "items" : []
-                } 
-    stores.append(new_store)
-
-    return jsonify(new_store)
-
-#GET /store/<string:name>
-@app.route("/store/<string:name>")
-def get_store(name):
+    store_data      = request.get_json()
     
-    for store in stores:
-        if store['name'] == name:
-            return jsonify(store)
-    
-    return jsonify({'message':"store not found"})
+    if 'name' not in store_data:
+        abort(400, "Bad request. Insure that 'name' is included in json payload.")
 
-#GET /store
+    if store_data['name'] in [store['name'] for store in stores.values() ]:
+        abort(400, "Store already exists")
+
+    store_id        = uuid.uuid4().hex
+    new_store       = {**store_data, "id":store_id} 
+    stores[store_id]= new_store
+
+    return new_store, 201
+
+@app.route("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id], 200
+    
+    except KeyError:
+        abort(404, "Store not found")
+    
 @app.route("/store")
 def get_stores():
-    return jsonify({"stores":stores})
+    return {"stores" : list( stores.values() ) }
 
-#POST /store/<string:name>/item {name:, price:}
-@app.route("/store/<string:name>/item", methods=["POST"])
-def create_store_item(name):
-    request_data = request.get_json()
 
-    for store in stores:
-        if store['name'] == name:
-            new_item = {"name":request_data['name'], 'price':request_data['price']}
-            store['items'].append( new_item )
-            return jsonify(new_item)
+@app.route("/store/item", methods=["POST"])
+def create_store_item():
+    item_data       = request.get_json()
 
-    return jsonify({"message":"store not found"})
-
-#GET /store/<string:name>/item
-@app.route("/store/<string:name>/item", methods=["POST"])
-def get_store_items(name):
-    for store in stores:
-        if store['name'] == name:
-            return jsonify({'items': store['items']})
+    if ('store_id' not in item_data or
+        'name' not in item_data or
+        'price' not in item_data
+        ):
+        abort(400, "Bad request. Insure that 'store_id', 'name' and 'price' is included in json payload")
     
-    return {"message":"store not found"}
+    for item in items.values():
+        if item['store_id'] == item_data['store_id'] and item['name'] == item_data['name']:
+            abort(400, "Item already exists.")
+
+    if item_data['store_id'] in stores:
+        item_id         = uuid.uuid4().hex 
+        new_item        = {**item_data, "id":item_id}
+        items[item_id]  = new_item
+        return new_item, 201
+
+    else:
+        abort(404, "Store not found")
+
+@app.route("/store/item", methods=["GET"])
+def get_store_items():
+
+    item_data   = request.get_json()
+    if 'store_id' not in item_data:
+        abort(404, "Bad request. Insure that 'store_id' is included in json payload")
+
+    store_id    = item_data['store_id'] 
+    if store_id in stores:
+        store_items = [item for item_id, item in items.items() 
+                            if item['store_id'] == store_id]
+        return {"items": store_items}, 200
+    
+    else:
+        abort(404, "Store not found")
 
 
 
